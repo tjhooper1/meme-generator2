b5bdb19d8b4f738cccdd76acc8234c7eb1e79e45
tjhooper1 <tomh386@gmail.com>
2018-09-08 14:05:07 -0400

    fixed loading image to canvas

--- app.js
+++ app.js
@@ -2,31 +2,25 @@ window.onload = function() {
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d");
     var fileInput = document.getElementById("image");
-    var img = new Image();
+    var img = document.createElement('img');
 
     fileInput.onchange = function(evt){
         var files = evt.target.files; // FileList object
 	    var file = files[0];
-	    
         var reader = new FileReader();
         // Read in the image file as a data URL.
-        reader.onload = function(evt){
-                img.src = evt.target.result;
-                reader.readAsDataURL(file);
-                context.drawImage(img,100,100);
-            
-	    }    
+        reader.onload = function(){
+                img.onload = function(){
+                    canvas.width = img.width;
+                    canvas.height = img.height;
+                    context.drawImage(img, 0,0);
+                }
+                img.src = reader.result;
+                console.log(img.src);
+                
+                
+            }    
+            reader.readAsDataURL(file);
     }
 };
 
-// var openFile = function(event) {
-//     var input = event.target;
-
-//     var reader = new FileReader();
-//     reader.onload = function(){
-//       var dataURL = reader.result;
-//       var output = document.getElementById('canvas');
-//       output.src = dataURL;
-//     };
-//     reader.readAsDataURL(input.files[0]);
-//   };
